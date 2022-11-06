bbf56bf2d7dc0b7455cb230ed9c3d8c5d11b8b07
DD7111 <davinadaji71@gmail.com>
2022-11-06 16:27:49 +0000

    worked on function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    newString = []
+    while (num > 0){
+        newString.push(string)
+        num -= 1
+    }
+    finalString = newString.join('')
+    return finalString
 };
 
 // Do not edit below this line
